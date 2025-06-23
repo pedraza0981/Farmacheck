4f974e20109070258ef3e1463424408ca72c0a5e
pedraza0981 <pedraza0981@hotmail.com>
2025-06-23 00:53:52 -0600

    homologacion
    alerts y homologacion

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showAlert(message, type = 'info') {
+    return Swal.fire({ text: message, icon: type });
+}
+
+function confirmAction(message) {
+    return Swal.fire({
+        text: message,
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonText: 'Aceptar',
+        cancelButtonText: 'Cancelar'
+    }).then(result => result.isConfirmed);
+}
